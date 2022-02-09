c04732a12c7f6123d6aae0cd98cb5e8cd5aa9349
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-02-09 17:50:10 +0900

    CSS Modulesの利用

---
+++ pages/articles/_id/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <dir>
+    <div>詳細ページ</div>
+    <div>
+      <nuxt-link to="/articles/10/edit">Nuxt Link</nuxt-link>
+    </div>
+
+    <div>
+      <a href="/articles/10/edit">a タグ </a>
+    </div>
+  </dir>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const foo = 'foo'
+    console.log(foo)
+  },
+}
+</script>
